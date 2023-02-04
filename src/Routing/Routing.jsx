6085f82285562ec0269e2b49ec630499cdb24a4f
beyondnaxos <86872427+beyondnaxos@users.noreply.github.com>
2023-02-04 13:27:53 +0100

    Creating comp for id

--- src/Routing/Routing.jsx
+++ src/Routing/Routing.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import React, { useContext } from 'react'
 import DashView from '../components/DashView/DashView'
+import IdChoose from '../components/IdChoose/IdChoose'
 import { AuthContext } from '../../service/context.js';
 // import Home from './components/Home/Home'
 
---
+++ src/components/IdChoose/IdChoose.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function IdChoose() {
+
+  return (
+    <div>
+      <input className='getid' type="text" placeholder="Enter your id"  />
+    </div>
+  )
+}
+
+export default IdChoose
